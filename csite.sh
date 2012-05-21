983443ec44349dfa6952ff51e8c401c865cdcec8
Solomon laptop <solomon.kahn@gmail.com>
2012-05-21 19:15:30 -0400

    little script to check if domain names are taken

---
+++ csite.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+availability () {
+  if whois "$1" | grep -q Registrant
+  then
+    echo "$1 is taken"
+  else
+    echo "$1 is available!!!"
+  fi
+}
+
+dotcom () {
+  availability "$1.com"
+}
+
+dotorg () {
+  availability "$1.org"
+}
+
+dotnet () {
+  availability "$1.net"
+}
+
+dotcom $1
+dotnet $1
+dotorg $1
